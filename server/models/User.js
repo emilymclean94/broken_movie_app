433eb6e4a3fe445499b723e2fbee25f774bef596
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-06-05 15:06:11 -0400

    filled out models

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+const movieScema = require("./Movies");
+const userSchema = new Schema(
+  {
+    firstname: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, match: [/.+@.+\..+/, 'Must match an email address!'], },
+    myList: [movieScema],
+    watched: [movieScema],
+    friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+userSchema
+  .virtual('friendCount')
+  .get(function () {
+    return `${this.friends.length}`;
+  })
+
+
+const User = model('User', userSchema);
+
+module.exports = User;
