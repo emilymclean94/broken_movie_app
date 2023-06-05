343ba44581534960829f125211157a6c843a8e5c
Keely Sherman <keelysherman@Keelys-MacBook-Pro.local>
2023-06-05 19:01:32 -0400

    movie search and movie results basic html

--- client/src/components/DashboardUser/MovieResults.js
+++ client/src/components/DashboardUser/MovieResults.js
@@ -1,3 +1,18 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import SearchResults from './SearchResults';
+import React from 'react';
+
+const SearchResults = ({ searchResults }) => {
+  return (
+    <div>
+        <h2>Search Results</h2>
+            {searchResults.map((result) => (
+                <div key={result.id}>
+                <img src={result.image} alt={result.title} />
+                <h3>{result.title}</h3>
+                <p>{result.description}</p>
+                </div>
+            ))}
+    </div>
+  );
+};
+
+export default SearchResults;
