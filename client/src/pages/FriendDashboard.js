25a7251c1c635cbf33ceaa60f6df1e4005feed38
Keely Sherman <keelysherman@Keelys-MacBook-Pro.local>
2023-06-05 19:25:16 -0400

    save

---
+++ client/src/pages/FriendDashboard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import NavBar from '../components/NavBars/NavBar'
+import FriendsProfile from '../components/DashboardFriend/FriendsProfile';
+import FriendsToWatch from '../components/DashboardFriend/FriendsToWatch';
+import FriendsWatchedList from '../components/DashboardFriend/FriendsWatchedList';
+import Footer from '../components/NavBars/Footer';
+
+
+
+const FriendDashboard = () => {
+    return (
+    <div>
+        <h1>Testing friends dashboard</h1>
+    <NavBar />
+    <FriendsProfile/>
+    <FriendsToWatch />
+    <FriendsWatchedList />
+    <Footer />
+    </div>
+    );
+    };
+    
+    export default FriendDashboard;
--- client/src/pages/MyDashboard.js
+++ client/src/pages/MyDashboard.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import SearchBar from './SearchBar';
-import SearchResults from './SearchResults';
+import SearchBar from '../components/DashboardUser/MovieSearch';
+import SearchResults from '../components/DashboardUser/MovieResults';
+import NavBar from '../components/NavBars/NavBar';
+import UserProfile from '../components/DashboardUser/UserProfile';
+import MyWatchedList from '../components/DashboardUser/MyWatchedList';
+import MyToWatch from '../components/DashboardUser/MyToWatch';
+ 
 
-const MyDash = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/profile" component={SearchBar} />
-        <Route exact path="/results" component={SearchResults} />
-      </Switch>
-    </Router>
-  );
+const MyDashboard = () => {
+return (
+<div>
+    <h1>Testing dashboard</h1>
+<NavBar />
+<UserProfile/>
+<SearchBar />
+<SearchResults />
+<MyToWatch />
+<MyWatchedList />
+</div>
+);
 };
 
-export default MyDash;
+export default MyDashboard;
