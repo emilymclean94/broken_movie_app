91b777dfd5fbafc1a212b69addd6da1e1a9c7118
Keely Sherman <keelysherman@Keelys-MacBook-Pro.local>
2023-06-06 09:53:59 -0400

    save from last night

--- client/src/components/NavBars/HomeNav.js
+++ client/src/components/NavBars/HomeNav.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 const HomeNav = () => {
     return (
-        <div>This is where our home nav is created</div>
+        <div>This is where our home nav is created
+            <nav>
+                <img src={logo} alt="logo" />
+                <h1>StreamVerse</h1>
+                <ul>
+                    <li><Login /></li>
+                    <li><SignUp /></li>
+                </ul>
+            </nav>
+        </div>
     )
 }
 export default HomeNav;
--- client/src/components/NavBars/NavBar.js
+++ client/src/components/NavBars/NavBar.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import logo from '../../../Assets/Icons/logo.png';
+import FriendsModal from '../ModalsPages/FriendsModal';
 
 const NavBar = () => {
     return (
-        <div> this is the nav bar we will use everywhere but the home page</div>
+        <div> this is the nav bar we will use everywhere but the home page
+            <nav>
+                <img src={logo} alt="logo" />
+                <h1>StreamVerse</h1>
+                <ul>
+                    <li>Profile</li>
+                    <li>Feed</li>
+                    <li><FriendsModal/></li>
+                    <li>Sign Out</li>
+                </ul>
+            </nav>
+            
+        </div>
+
     )
 }
 export default NavBar;
