f08bde20480dc6ff1aece91a60833b1394d57379
Keely Sherman <keelysherman@Keelys-MacBook-Pro.local>
2023-06-05 20:10:42 -0400

    added stuff

--- client/src/components/HomePages/Hero.js
+++ client/src/components/HomePages/Hero.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import space from '../../Assets/images/space.mp4';
 
 const Hero = () => {
     return (
-        <div>this is our hero section</div>
-    )
+        <div>
+            <video src={space} className="w-full h-screen object-cover" autoPlay loop muted></video>
+        </div>
+    );
 }
 export default Hero;
--- client/src/components/HomePages/Scroll1.js
+++ client/src/components/HomePages/Scroll1.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const Scroll = () => {
+const Scroll1 = () => {
     return (
         <div>this will be the scroll animation</div>
     )
 }
+export default Scroll1;
