905e2a00fb1dc265c827c991113dc0e793c72b56
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-06-06 11:03:35 -0400

    added utils in client side

---
+++ client/src/utils/queries.js
@@ -0,0 +1,46 @@
+import { gql } from '@apollo/client';
+
+//! refactoring in progress
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_MOVIES = gql`
+  query getThoughts {
+    thoughts {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+    }
+  }
+`;
+
+export const QUERY_SINGLE_MOVIE = gql`
+  query getSingleThought($thoughtId: ID!) {
+    thought(thoughtId: $thoughtId) {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
+    }
+  }
+`;
