9c5990d6ada3bdaf6a70a834e88cd584944c7bc9
Emily McLean <emilymclean93@gmail.com>
2023-06-06 12:34:43 -0400

    movie input type and started mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -14,20 +14,53 @@ const typeDefs = gql`
   }
 
   type Movie {
+    movieId: ID
     posterImg: String
     title: String
     releaseDate: String
     description: String
-    adult: Boolean
+    rated: Boolean
     rating: String
     createdAt: Date
 }
 
+type movieInput {
+  movieId: String
+  movieId: ID
+    posterImg: String
+    title: String
+    releaseDate: String
+    description: String
+    rated: Boolean
+    rating: String
+    createdAt: Date
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+
   # Define which queries the front end is allowed to make and what data is returned
+  
   
   type Query {
     users: [User]
+    movies: [Movie]
   }
+
+  type Mutation {
+    adduser (
+      firstname: String!
+      username: String
+      avatar: String
+      email: String
+    )
+    // remove user?
+    // login
+    // add movie (to watched)
+    // remove movie (from watched)
+}
 `;
 
 module.exports = typeDefs;
