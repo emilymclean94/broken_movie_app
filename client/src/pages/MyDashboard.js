11b43df3f63b7c412846e8b1bb1924204c0406ca
Emily McLean <emilymclean93@gmail.com>
2023-06-07 09:35:22 -0400

    added rough outline of user avatar and bio section to uesr dashboard

--- client/src/pages/MyDashboard.js
+++ client/src/pages/MyDashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from '../components/NavBars/NavBar';
+import Footer from '../components/NavBars/Footer';
 import UserProfile from '../components/DashboardUser/UserProfile';
 // import SearchBar from '../components/DashboardUser/MovieSearch';
 // import SearchResults from '../components/DashboardUser/MovieResults';
