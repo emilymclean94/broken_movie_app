651a165f156279ce0e163d96a3dcc751bd59dea5
EGARRISXN <egarrisxn@gmail.com>
2023-06-07 18:09:43 -0400

    formatting for movies updated.

--- client/src/pages/Movies/MovieBox.js
+++ client/src/pages/Movies/MovieBox.js
@@ -2,11 +2,10 @@ import React, { useState } from 'react';
 import "./MovieAPI.css";
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
-import { styled } from '@mui/system';
 
-const API_IMG="https://image.tmdb.org/t/p/w500/";
+const API_IMG = 'https://image.tmdb.org/t/p/w500/';
 
-const useStyles = styled('div')(({ theme }) => ({
+const styles = ({ theme }) => ({
   modal: {
     display: 'flex',
     alignItems: 'center',
@@ -17,10 +16,9 @@ const useStyles = styled('div')(({ theme }) => ({
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
-}));
+});
 
-const MovieBox = ({ titel, poster_path, vote_average, release_date, overview }) => {
-  const classes = useStyles();
+const MovieBox = ({ title, poster_path, vote_average, release_date, overview }) => {
   const [show, setShow] = useState(false);
 
   const handleShow = () => {
@@ -40,15 +38,15 @@ const MovieBox = ({ titel, poster_path, vote_average, release_date, overview }) 
             View More
           </button>
           <Modal
-            className={classes.modal}
+            className={styles.modal}
             open={show}
             onClose={handleClose}
             aria-labelledby="modal-title"
             aria-describedby="modal-description"
           >
-            <div className={classes.paper}>
+            <div className={styles.paper}>
               <img className="card-img-top" style={{ width: '14rem' }} src={API_IMG + poster_path} alt="movie" />
-              <h3>{titel}</h3>
+              <h3>{title}</h3>
               <h4>IMDb: {vote_average}</h4>
               <h5>Release Date: {release_date}</h5>
               <br />
