473dfbf05b5dd070c2068a0a794ceef8e026df3f
Sami <coolsweetsami@gmail.com>
2023-06-07 18:31:51 -0400

    updates to login info

--- client/src/components/ModalsPages/LoginModal.js
+++ client/src/components/ModalsPages/LoginModal.js
@@ -59,13 +59,6 @@ const LogIn = () => {
                     <form onSubmit={handleFormSubmit} className="modal-box">
                         <Button htmlFor="my-modal-3" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</Button>
                         <div>
-                            <TextField label="Your email"
-                                sx={{ m: 1, width: '25ch' }}
-                                id="email1"
-                                placeholder="janedoe@email.com"
-                                required
-                                type="email"
-                            />
                             <TextField label="Username"
                                 sx={{ m: 1, width: '25ch' }}
                                 id="username1"
