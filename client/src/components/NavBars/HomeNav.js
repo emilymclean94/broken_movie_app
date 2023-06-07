c23fe3b13c77449dd53ec3403e8778a7f2a85cbb
Sami <coolsweetsami@gmail.com>
2023-06-07 12:44:30 -0400

    readme description

--- client/src/components/NavBars/HomeNav.js
+++ client/src/components/NavBars/HomeNav.js
@@ -39,6 +39,7 @@ function HomeNav() {
           >
             StreamVerse
           </Typography>
+          
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
               <Button
