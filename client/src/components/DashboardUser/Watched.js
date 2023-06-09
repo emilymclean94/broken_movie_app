7ff0cc8ce52f5520cb55adef87825f70001f1908
keelyybug <keelysherman00@gmail.com>
2023-06-09 11:46:59 -0400

    friends dashboard, watch list, to watch, and watched html updated

--- client/src/components/DashboardUser/Watched.js
+++ client/src/components/DashboardUser/Watched.js
@@ -3,20 +3,23 @@ import styled from '@emotion/styled';
 import Avatar4 from '../Assets/AvatarImages/Avatar4.png';
 const API_IMG = 'https://image.tmdb.org/t/p/w500/';
 
-const Rated = (id) => {
-
+const Rated = ({id, title, poster_path}) => {
+//delete button function
+// function that pulls rating value from database to input into value
 
   return (
     <div>
-          <MyWatchedContainer key={MyWatchedList.length}>
-    <Poster src={API_IMG + poster_path} alt="movie poster" />
-        <Title>{title}</Title>
-        <RateContainer>
-        <Typography component="legend">Read only</Typography>
-        <Rating name="read-only" value={value} readOnly />
-        </RateContainer>
-        <DeleteMovieButton>Delete</DeleteMovieButton>
-    </MyWatchedContainer>;
+      {searchResults.map((result) => (
+        <MyWatchedContainer key={MyWatchedList.length}>
+          <Poster src={API_IMG + poster_path} alt="movie poster" />
+          <Title>{title}</Title>
+            <RateContainer>
+              <Typography component="legend">(pull stored label if possible)if not i will scratch this label</Typography>
+              <Rating name="read-only" value={value} readOnly />
+            </RateContainer>
+          <DeleteMovieButton>Delete</DeleteMovieButton>
+        </MyWatchedContainer>
+    ))}
     </div>
   );
 };
