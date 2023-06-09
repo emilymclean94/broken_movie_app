ea02875c7d7674ab940e24b2034c83c66bf16683
Emily McLean <emilymclean93@gmail.com>
2023-06-08 21:07:16 -0400

    added imports to MovieSearch.js and created localSotrage.js with functions to save movie IDs

--- client/src/components/DashboardUser/MovieSearch.js
+++ client/src/components/DashboardUser/MovieSearch.js
@@ -1,27 +1,29 @@
-// import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Auth from '../../utils/auth';
+import { useMutation } from "@apollo/client";
 
-// const SearchBar = () => {
-//     const [searchTerm, setSearchTerm] = useState('');
-//     const [suggestions, setSuggestions] = useState([]);
+const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [suggestions, setSuggestions] = useState([]);
 
 
-//     return (
-//     <form onSubmit={} className="flex flex-col">
-//         <input type="text" placeholder="Search" value={searchTerm} onChange={} className="px-4 py-2 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-//             {suggestions.length > 0 && (
-//                 <div className="">
-//                     {suggestions.map((suggestion) => (
-//                     <button key={suggestion.id} className="px-4 py-2 text-left hover:bg-gray-100 focus:outline-none" onClick={() => {}}>
-//                         {suggestion.title}
-//                     </button>
-//             ))}
-//             </div>
-//         )}
-//         <button type="submit" className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
-//         Search
-//         </button>
-//     </form>
-//     );
-// };
+    return (
+    <form onSubmit={} className="flex flex-col">
+        <input type="text" placeholder="Search" value={searchTerm} onChange={} className="px-4 py-2 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+            {suggestions.length > 0 && (
+                <div className="">
+                    {suggestions.map((suggestion) => (
+                    <button key={suggestion.id} className="px-4 py-2 text-left hover:bg-gray-100 focus:outline-none" onClick={() => {}}>
+                        {suggestion.title}
+                    </button>
+            ))}
+            </div>
+        )}
+        <button type="submit" className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
+        Search
+        </button>
+    </form>
+    );
+};
 
-// export default SearchBar;
+export default SearchBar;
