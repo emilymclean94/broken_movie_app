38a0a29ef7cf241a49e34245ffd4bcf697956ee2
keelyybug <keelysherman00@gmail.com>
2023-06-09 10:50:49 -0400

    help from john

--- client/src/components/DashboardUser/AddToMyWatch.js
+++ client/src/components/DashboardUser/AddToMyWatch.js
@@ -11,11 +11,13 @@ const API_IMG = 'https://image.tmdb.org/t/p/w500/';
 
 
 const AddToMyToWatch = ({ title, poster_path, id }) => {
-  const [myMovieList, setMovieList] = useState([]);
+//  return and map over the div you created
+//  pull information from data base
+//onclick function that stores your rating into the data base(add ratings to database)
 
-  const handleAddMovie = (event, id) => {
-    const newMovie = (
-      <MovieContainer key={myMovieList.length}>
+
+  return (
+<MovieContainer key={myMovieList.length}>
         <MoviePoster src={API_IMG + poster_path} alt="movie poster" />
         <MovieTitle>{title}</MovieTitle>
         {/* <Rating
@@ -35,18 +37,9 @@ const AddToMyToWatch = ({ title, poster_path, id }) => {
           {value !== null && (
             <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
           )}
-        <Rate /> */}
+        
+<button onClick={handleAddMyWatched(id)}>Rate</button> */}
       </MovieContainer>
-    );
-
-    setMovieList((prevMovieList) => [...prevMovieList, newMovie]);
-  };
-
-
-  return (
-    <div>
-      <button onClick={handleAddMovie(id)}>Add to List</button>
-    </div>
   );
 };
 
